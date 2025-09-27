ad3b3018f4be29f7c7c7c61dc63494b39d38298f
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2025-09-27 12:40:17 -0300

    Refactor code structure for improved readability and maintainability

--- src/sections/Inicio.tsx
+++ src/sections/Inicio.tsx
@@ -9,9 +9,9 @@ const Inicio = () => {
   return (
     <section
       id="inicio"
-      className="relative min-h-screen flex items-center justify-center bg-[url('images/bg.jpg')] bg-cover bg-center bg-no-repeat"
+      className="relative min-h-screen flex items-center justify-center bg-[url('images/bg-light.webp')] dark:bg-[url('images/bg-dark.webp')] bg-cover bg-center bg-no-repeat"
     >
-      <div className="absolute inset-0 z-0 bg-gradient-to-br from-gray-200/80 to-gray-300/80 dark:from-gray-900/60 dark:to-gray-800/60"></div>
+      <div className="absolute inset-0 z-0 bg-gradient-to-br from-gray-200/60 to-gray-300/60 dark:from-gray-900/60 dark:to-gray-800/60"></div>
 
       <div className="container mx-auto px-6 mt-25 sm:mt-14 md:mt-10 text-center relative z-10">
         <div className="max-w-4xl mx-auto">
